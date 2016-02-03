e10b0d30cbe4d597c862a80c7c9f91f5e235675e
Judge Dredd (key 6E23685A) <the.guard@mail.ru>
2016-02-03 12:43:30 -0700

    fix bug in bruteforce prob1 b (hw1)

--- test/brute_mat.py
+++ test/brute_mat.py
@@ -8,9 +8,9 @@ while r <= r1 + r2 + 0.5:
     A = numpy.random.random_integers(1, 10, (2, 2))
     B = numpy.random.random_integers(1, 10, (2, 2))
 
-    r1 = numpy.amax(numpy.linalg.eigvals(A))
-    r2 = numpy.amax(numpy.linalg.eigvals(B))
-    r = numpy.amax(numpy.linalg.eigvals(A + B))
+    r1 = numpy.amax([abs(x) for x in numpy.linalg.eigvals(A)])
+    r2 = numpy.amax([abs(x) for x in numpy.linalg.eigvals(B)])
+    r = numpy.amax([abs(x) for x in numpy.linalg.eigvals(A + B)])
 
 
 print(r, r1 + r2, r1, r2)
