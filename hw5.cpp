559c3e5a10db41e41b78e6b7a6e11e0c03094c98
Judge Dredd (key 6E23685A) <the.guard@mail.ru>
2016-03-26 17:02:34 -0600

    fixed a bug + plotscript (hw5)

--- hw5.cpp
+++ hw5.cpp
@@ -30,7 +30,7 @@ double Error(const std::vector<double> &v) {
 	double e = 0;
 	for (auto &x : v) e += x * x;
 
-	return sqrt(e/v.size());
+	return sqrt(e);
 }
 
 double A(double x) {
@@ -40,8 +40,8 @@ double A(double x) {
 // l'
 double dl(unsigned n, double h, double x) {
 	if (x > (n + 1) * h || x < (n - 1) * h) return 0;
-	if (x > n * h) return -1;
-	return 1;
+	if (x > n * h) return -1/h;
+	return 1/h;
 }
 
 // map [-1,1] into [a,b]
@@ -59,19 +59,22 @@ double GaussQuad(unsigned N, unsigned i, unsigned j, double h) {
 	double x1 = coor_map(a, b, -1/sqrt(3));
 	double x2 = coor_map(a, b, 1/sqrt(3));
 
+#if 0
 	double e = 0;
 	for (double k = a; k < b; k += h*h) {
-		e += (A(k) * dl(i, h, k) * dl(j, h, k)
-		      + A((k + h*h)) * dl(i, h, k + h*h)
-		      * dl(j, h, k + h*h)) / 2;
+		e += h*h*(A(k) * dl(i, h, k) * dl(j, h, k)
+			  + A((k + h*h)) * dl(i, h, k + h*h)
+			  * dl(j, h, k + h*h)) / 2;
 	}
-	return (b - a) * e;
+	return e;
 
-#if 0
+#else
 	return (b - a)/2 * (A(x1) * dl(i, h, x1) * dl(j, h, x1)
 			    + A(x2) * dl(i, h, x2) * dl(j, h, x2));
 
+#endif
 
+#if 0
 	if (i == N - 1) {
 		return (b - a)/2 * A(x1) * dl(i, h, x1) * dl(j, h, x1);
 	} else {
@@ -107,7 +110,7 @@ void Problem_3() {
 	double e = 1e-10;
 	unsigned steps;
 
-	for (double h = 0.1; h >= 0.025; h /= 2) {
+	for (double h = 0.5; h >= 0.025; h /= 2) {
 		unsigned N = static_cast<unsigned> (floor(1/h));
 		// rhs
 		std::vector<double> b(N - 1, 0);
